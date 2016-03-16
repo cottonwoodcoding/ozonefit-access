5ee96080c8799027114d077dff4bef8e80864f43
Jake Sorce <jakesorce@gmail.com>
2016-03-15 21:42:48 -0600

    fix admin force ozone challenge

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,19 +1,25 @@
 class DashboardController < ApplicationController  
   include TimeParser
+  before_action :force_ozone_challenge
 
   def index
-    if current_user.initial_ochallenge_completed && !current_user.admin
-      @video_url = SoundCloud.random
-      @motivation = Motivation.all.try(:sample)
-      @current_day = Day.find(Date.today.wday)
-      @workout =  Workout.find_by(id: @current_day.workout_id)
-      if @workout
-        @workout_min = TimeParser.format_time(@workout.min_time)
-        @workout_ozf = TimeParser.format_time(@workout.ozf_time)
-      end
-    else
-      flash[:error] = 'You need to complete an initial O.Zone Challenge!'
-      redirect_to challenge_path
+    @video_url = SoundCloud.random
+    @motivation = Motivation.all.try(:sample)
+    @current_day = Day.find(Date.today.wday)
+    @workout =  Workout.find_by(id: @current_day.workout_id)
+    if @workout
+      @workout_min = TimeParser.format_time(@workout.min_time)
+      @workout_ozf = TimeParser.format_time(@workout.ozf_time)
     end
   end
+
+  private
+    def force_ozone_challenge
+      unless current_user.admin
+        unless current_user.initial_ochallenge_completed
+          flash[:error] = 'You need to complete an initial O.Zone Challenge!'
+          redirect_to(challenge_path) and return
+        end
+      end
+    end
 end
